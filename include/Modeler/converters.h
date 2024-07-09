9d379923a5bd466aa1b669912997fd0a2e1c224e
IslamAAli <islam.a.mustafa@gmail.com>
2024-07-09 12:35:36 -0600

    adding helper header file

--- include/Modeler/converters.h
+++ include/Modeler/converters.h
@@ -1,6 +1,12 @@
 #ifndef __CONVERTERS_H
 #define __CONVERTERS_H
 
+#include <opencv2/core/core.hpp>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <Eigen/Dense>
+
+
 namespace CARV_HELPERS
 {
     cv::Mat SFMTranscriptInterface_ORBSLAM::se3ToCvMat(const Sophus::SE3<float>& se3);
